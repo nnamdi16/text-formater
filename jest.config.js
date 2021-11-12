20726f9d712c1e4d5639405b68ded41a29db3ca9
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-11-13 00:33:03 +0100

    Added multiple jest configuration and added more configurations tp jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    verbose: true,
+    roots: ['src'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+
 };
