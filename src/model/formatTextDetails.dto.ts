d1b864a2e1fdd28b893dc98d4800a4bc727da74d
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-11-15 00:48:19 +0100

    Created a DTO that houses all the interfaces

---
+++ src/model/formatTextDetails.dto.ts
@@ -0,0 +1,47 @@
+export interface IRandomJokesResponse {
+  readonly icon_url: string;
+  readonly id: string;
+  readonly url: string;
+  readonly value: string;
+}
+
+export interface IFormatString {
+  readonly text: string;
+  readonly boldStrings: string[];
+  readonly italicsStrings: string[];
+  readonly replaceStrings: IReplaceString;
+
+
+}
+
+export interface IReplaceString {
+  [key:string] : string
+}
+
+export interface IFormatText extends IFormatString{
+  readonly lineWidth: number;
+  readonly textAlignment: string;
+  readonly lineSpacing: string;
+  readonly randomJokesIdentifier:string[]
+
+}
+
+export interface IFormatTextResponse {
+  lineSentence: string;
+  sentenceArray: string[],
+  splittedText: string[],
+  index: number;
+}
+
+
+export interface IFormatTextParameter extends IFormatText, IFormatTextResponse {
+  formattedString: string;
+  sentenceWidthCounter: number;
+  randomJokeCounter: number;
+}
+
+export interface IHandler {
+  setNext(handler: IHandler): IHandler;
+  handle(request:IFormatTextParameter): Promise<IFormatTextResponse> 
+}
+
