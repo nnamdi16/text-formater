0e99eb45a7bb606e5771e2b7de1393acc1806599
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-11-14 22:56:44 +0100

    Implemented formatTextHandler to capture different scenarios in fromatting a text

---
+++ src/service/formatTextHandler.service.ts
@@ -0,0 +1,81 @@
+import { IFormatTextParameter, IHandler, IFormatTextResponse } from './../model/textFormatDetails.dto';
+import { alignText, fetchRandomJoke } from './../util/util';
+
+
+
+abstract class AbstractHandler implements IHandler {
+  private nextHandler!: IHandler;
+  public setNext(handler: IHandler): IHandler {
+    this.nextHandler = handler;
+    
+    return handler;
+  }
+
+   public async handle(request: IFormatTextParameter): Promise<IFormatTextResponse> {
+    if (this.nextHandler) {
+      return this.nextHandler.handle(request)
+    }
+    return {lineSentence:request.lineSentence, sentenceArray:request.sentenceArray,splittedText:request.splittedText, index:request.index}
+  }
+  
+}
+
+export class FormatTextWithNewLineHandler extends AbstractHandler {
+   public async handle(request: IFormatTextParameter): Promise<IFormatTextResponse> {
+     if (request.formattedString?.indexOf("\n") > -1) {
+       request.lineSentence += `${request.formattedString.substring(0, request.formattedString.indexOf("\n"))}`;
+      //  console.log(request.lineSentence);
+      request.sentenceArray?.push(alignText(request.lineSentence?.trim(), request.lineWidth, request.textAlign));
+      request.sentenceWidthCounter = 0;
+      if (request.randomJokeCounter > 0) {
+        const unformattedStrings: string[] = request.splittedText.slice(request.index + 1);
+        const randomJoke = await fetchRandomJoke();
+        if (randomJoke.length > 0) {
+          randomJoke.push(request.formattedString.substring(request.formattedString.indexOf("\n") + 1));
+          request.splittedText = randomJoke.concat(unformattedStrings);
+          request.index = -1;
+        }
+        request.randomJokeCounter = 0;
+        request.lineSentence = '';
+        return {lineSentence:request.lineSentence, sentenceArray:request.sentenceArray,splittedText:request.splittedText, index: request.index}
+      } else {
+        request.lineSentence = request.formattedString.substring(request.formattedString.indexOf("\n") + 1);
+        return {lineSentence:request.lineSentence, sentenceArray:request.sentenceArray, splittedText: request.splittedText, index:request.index}
+      }
+    }
+    return super.handle(request)
+  }
+  
+}
+
+export class FormatTextBelowLineWidthHandler extends AbstractHandler {
+
+  public async handle(request: IFormatTextParameter): Promise<IFormatTextResponse> {
+    if (request.sentenceWidthCounter < request.lineWidth &&  (request.sentenceWidthCounter + request.formattedString.length) <= request.lineWidth) {
+      request.lineSentence += ` ${request.formattedString}`;
+      return {lineSentence:request.lineSentence, sentenceArray:request.sentenceArray, splittedText: request.splittedText,index: request.index}
+    }
+    return super.handle(request)
+  }
+}
+
+
+export class FormatTextAboveLineWidthHandler extends AbstractHandler {
+  public async  handle(request: IFormatTextParameter): Promise<IFormatTextResponse> {
+ if ((request.sentenceWidthCounter + request.formattedString.length) > request.lineWidth) {
+  request.sentenceArray?.push(alignText(request.lineSentence?.trim(), request.lineWidth, request.textAlign));
+  request.lineSentence = ` ${request.formattedString}`;
+  request.sentenceWidthCounter = 0;
+  return {lineSentence:request.lineSentence, sentenceArray:request.sentenceArray, splittedText:request.splittedText, index:request.index}
+ }
+ return super.handle(request);
+  }
+}
+
+
+
+
+
+
+
+
